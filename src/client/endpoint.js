7f1ae68ec756854650a6957c63c101d3f3bb7ff4
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-03-24 19:48:30 +0900

    `EndPoint.js`を作成
    基本的なファイルとディレクトリ構造を作成しました。

---
+++ src/client/endpoint.js
@@ -0,0 +1,27 @@
+/*!
+ * EndPoint.js
+ * Copyright 2023 ActiveTK. All rights reserved.
+ * Released under the MIT license
+ */
+
+"use strict";
+
+(function(window, undefined) {
+
+  window.endpointjs = function( ...callback ) {
+
+    let result = {};
+
+    result.UserAgent = navigator.userAgent;
+
+    for (let i = 0 ; i < callback.length ; i++)
+    {
+      if (typeof callback[i] === 'function')
+        callback[i](result);
+      else
+        console.error(i + "番目の引数に関数以外が指定されました。");
+    }
+
+  }
+
+}(window);
