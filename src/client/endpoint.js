66149092bff3f2da43e9ccaddcc425d7b55da44a
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-03-25 12:40:28 +0900

    fix async code

--- src/client/endpoint.js
+++ src/client/endpoint.js
@@ -28,48 +28,51 @@
       result.Headers.AcceptLanguage = data.AcceptLang;
       result.Headers.AcceptEncoding = data.AcceptEncode;
       result.Headers.UserAgentClientHints = data.UserAgentClientHints;
+    })
+    .then(() => {
+      // WebRTC
+      window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+      let rtc = new RTCPeerConnection({iceServers:[]}), noop = function(){};
+      rtc.createDataChannel('');
+      rtc.createOffer(rtc.setLocalDescription.bind(rtc), noop);
+      rtc.onicecandidate = function(ice) {
+        if (ice && ice.candidate && ice.candidate.candidate) {
+          result.WebRTCInfo = ice.candidate.candidate;
+          try {
+            result.PrivateIPaddress = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+          } catch { }
+          rtc.onicecandidate = noop;
+        }
+        getBrowserInfo();
+        runCallback();
+      }
     });
-
-    // WebRTC
-    window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-    let rtc = new RTCPeerConnection({iceServers:[]}), noop = function(){};
-    rtc.createDataChannel('');
-    rtc.createOffer(rtc.setLocalDescription.bind(rtc), noop);
-    rtc.onicecandidate = function(ice) {
-      if (ice && ice.candidate && ice.candidate.candidate) {
-        result.WebRTCInfo = ice.candidate.candidate;
-        try {
-          result.PrivateIPaddress = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
-        } catch { }
-        rtc.onicecandidate = noop;
+    function getBrowserInfo() {
+      // ブラウザについての情報を取得
+      result.Browser.CodeName = location.appCodeName;
+      result.Browser.Name = navigator.appName;
+      result.Browser.Version = navigator.appVersion;
+      result.Browser.Language = navigator.language;
+      result.Browser.Platform = navigator.platform;
+      result.Browser.Referrer = document.referrer;
+      result.Browser.ScreenWidth = screen.width;
+      result.Browser.ScreenHeight = screen.height;
+      result.Browser.ScreenColorDepth = screen.colorDepth + "bit";
+      result.Browser.ViewPortWidth  =  window.innerWidth;
+      result.Browser.ViewPortHeight  =  window.innerHeight;
+      result.Browser.DevicePixelRatio  =  window.devicePixelRatio;
+      result.Browser.HasPointer  =  navigator.pointerEnabled;
+      result.Browser.MaxTouchPoints  =  navigator.maxTouchPoints;
+    }
+    function runCallback(){
+      // callback実行
+      for (let i = 0 ; i < callback.length ; i++)
+      {
+        if (typeof callback[i] === 'function')
+          callback[i](result);
+        else
+          console.error(i + "番目の引数に関数以外が指定されました。");
       }
     }
-
-    // ブラウザについての情報を取得
-    result.Browser.CodeName = location.appCodeName;
-    result.Browser.Name = navigator.appName;
-    result.Browser.Version = navigator.appVersion;
-    result.Browser.Language = navigator.language;
-    result.Browser.Platform = navigator.platform;
-    result.Browser.Referrer = document.referrer;
-    result.Browser.ScreenWidth = screen.width;
-    result.Browser.ScreenHeight = screen.height;
-    result.Browser.ScreenColorDepth = screen.colorDepth + "bit";
-    result.Browser.ViewPortWidth  =  window.innerWidth;
-    result.Browser.ViewPortHeight  =  window.innerHeight;
-    result.Browser.DevicePixelRatio  =  window.devicePixelRatio;
-    result.Browser.HasPointer  =  navigator.pointerEnabled;
-    result.Browser.MaxTouchPoints  =  navigator.maxTouchPoints;
-
-    // callback実行
-    for (let i = 0 ; i < callback.length ; i++)
-    {
-      if (typeof callback[i] === 'function')
-        callback[i](result);
-      else
-        console.error(i + "番目の引数に関数以外が指定されました。");
-    }
-
   }
-
 }(window));
